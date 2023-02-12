8b504f6f043680487a62d142e7cffd095ce3c144
Roman Dodenko <creed6695@mail.ru>
2023-02-12 18:49:04 +0700

    Новая версия

--- js/main.js
+++ js/main.js
@@ -2,223 +2,302 @@
 
 window.onload = function () {
 
-  document.addEventListener("click", function (e) {
-  const elementTarget = e.target;
+  gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
 
-  if (elementTarget.closest(".portfolio-swiper__button")) {
-    document.querySelectorAll(".portfolio-swiper__descr").forEach(function (s) {
-      s.classList.remove("portfolio-swiper-descr-active")
+  if (ScrollTrigger.isTouch !== 1) {
+
+    ScrollSmoother.create({
+      wrapper: '.page',
+      content: '.scroll',
+      smooth: 1.5,
+      effects: true
     })
-    elementTarget.closest(".portfolio-swiper__descr").classList.add("portfolio-swiper-descr-active")
-  }
-  if (!elementTarget.closest(".portfolio-swiper-descr-active")) {
-    document.querySelectorAll(".portfolio-swiper__descr").forEach(function (s) {
-      s.classList.remove("portfolio-swiper-descr-active")
+
+    gsap.fromTo('.hello', {
+      opacity: 1
+    }, {
+      opacity: 0,
+      scrollTrigger: {
+        trigger: '.hello',
+        start: 'center',
+        end: '620',
+        scrub: true
+      }
     })
-  }
-})
 
-const welcome = document.querySelector(".welcome");
+    gsap.fromTo('.about', {
+      opacity: .1,
+      x: -100,
+    }, {
+      opacity: 1,
+      x: 0,
+      scrollTrigger: {
+        trigger: '.about',
+        start: '-450',
+        end: '-200',
+        scrub: true,
+      }
+    })
 
-if (welcome) {
-  function welcomeDisabled() {
-    welcome.classList.add("welcome-disabled")
-  }
-  setTimeout(welcomeDisabled, 2000)
-}
+    gsap.fromTo('.portfolio', {
+      opacity: .1,
+      x: 100,
+    }, {
+      opacity: 1,
+      x: 0,
+      scrollTrigger: {
+        trigger: '.portfolio',
+        start: '-600',
+        end: '-180',
+        scrub: true
+      }
+    })
 
-const mainScreen = document.querySelector(".main-screen");
-const mainScreenTextInit = document.querySelector(".main-screen__text");
-const mainScreenText = document.querySelectorAll(".main-screen__text");
-const mainScreenMouse = document.querySelector(".main-screen-mouse")
-if (mainScreen) {
+    gsap.fromTo('.contacts', {
+      opacity: .1,
+    }, {
+      opacity: 1,
+      scrollTrigger: {
+        trigger: '.contacts',
+        start: '-600',
+        end: '-180',
+        scrub: true
+      }
+    })
 
-  document.addEventListener("scroll", function () {
-    let mainScreenTextInit = Math.floor(scrollY);
-    if (mainScreenTextInit >= 50) {
-      mainScreenText.forEach(function (e) {
-        e.style.color = "#000000";
+    gsap.fromTo('.contacts__list', {
+      y: 50,
+    }, {
+      y: 0,
+      scrollTrigger: {
+        trigger: '.contacts',
+        start: '-600',
+        end: '-180',
+        scrub: true
+      }
+    })
+
+    gsap.fromTo('.contacts__image', {
+      y: -50,
+    }, {
+      y: 0,
+      scrollTrigger: {
+        trigger: '.contacts',
+        start: '-600',
+        end: '-180',
+        scrub: true
+      }
+    })
+
+    let itemsT = gsap.utils.toArray('.portfolio-list__title')
+
+    itemsT.forEach(item => {
+      gsap.fromTo(item, {
+        opacity: .1
+      }, {
+        opacity: 1,
+        y: 0,
+        scrollTrigger: {
+          trigger: item,
+          start: '-820',
+          end: '-320',
+          scrub: true
+        }
       })
-      mainScreenMouse.style.cssText = `
-      opacity: 0;
-      visibility: hidden;
-    `;
+    })
+
+    let itemsA = gsap.utils.toArray('.about__item')
+
+    itemsA.forEach(item => {
+      gsap.fromTo(item, {
+        opacity: 0.1,
+        y: 20
+      }, {
+        opacity: 1,
+        y: 0,
+        scrollTrigger: {
+          trigger: item,
+          start: '-650',
+          end: '-550',
+          scrub: true
+        }
+      })
+    })
+
+    let itemsL = gsap.utils.toArray('.portfolio-swiper__image')
+
+    itemsL.forEach(item => {
+      gsap.fromTo(item, {
+        opacity: 0.1,
+        x: -30
+      }, {
+        opacity: 1,
+        x: 0,
+        scrollTrigger: {
+          trigger: item,
+          start: '-550',
+          end: '-380',
+          scrub: true
+        }
+      })
+    })
+
+    let itemsR = gsap.utils.toArray('.portfolio-swiper__descr')
+
+    itemsR.forEach(item => {
+      gsap.fromTo(item, {
+        opacity: 0.1,
+        x: 30
+      }, {
+        opacity: 1,
+        x: 0,
+        scrollTrigger: {
+          trigger: item,
+          start: '-550',
+          end: '-380',
+          scrub: true
+        }
+      })
+    })
+
+  }
+
+  let anim = gsap.timeline({})
+
+  anim
+    .fromTo(".hello__title", {
+      opacity: 0,
+      y: 50,
+    }, {
+      opacity: 1,
+      y: 0,
+      duration: .6,
+    })
+
+  anim
+    .fromTo(".hello__text", {
+        opacity: 0,
+        y: 50,
+      }, {
+        opacity: 1,
+        y: 0,
+        duration: .6,
+      },
+      "-=.3"
+    )
+
+  anim
+    .fromTo(".hello__item_1", {
+        opacity: 0,
+      }, {
+        duration: .6,
+        opacity: 1,
+      },
+      "-=.4"
+    )
+  anim
+    .fromTo(".hello__item_2", {
+        opacity: 0,
+      }, {
+        duration: .6,
+        opacity: 1,
+      },
+      "-=.4"
+    )
+  anim
+    .fromTo(".hello__item_3", {
+        opacity: 0,
+      }, {
+        duration: .6,
+        opacity: 1,
+      },
+      "-=.4"
+    )
+
+  anim
+    .fromTo(".hello__image", {
+        opacity: 0,
+        x: 50,
+      }, {
+        duration: .6,
+        opacity: 1,
+        x: 0,
+      },
+      "-=1.2"
+    )
+
+  function testWebP(callback) {
+    var webP = new Image();
+
+    webP.onload = webP.onerror = function () {
+      callback(webP.height == 2);
+    };
+
+    webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+  }
+
+  testWebP(function (support) {
+    if (support == true) {
+      document.querySelector('body').classList.add('webp');
     } else {
-      mainScreenText.forEach(function (e) {
-        e.style.color = "#ffffff";
-      })
-      mainScreenMouse.style.cssText = `
-    opacity: 1;
-    visibility: visible;
-  `;
+      document.querySelector('body').classList.add('no-webp');
     }
-  })
-
-  createPosition();
-
-  document.addEventListener("scroll", createPosition)
-
-  function createPosition() {
-    const contentElement = document.querySelector(".hello__container");
-    const windowHeight = window.innerHeight;
-
-    const finalPosition = scrollY / (contentElement.offsetTop - windowHeight) * 5;
-
-    finalPosition < 100 ? forestAnimation(finalPosition) : forestAnimation(100);
-  }
-
-  function forestAnimation(finalPosition) {
-    const bigImage = document.querySelector(".main-screen__big-image");
-    const textsBlocks = document.querySelectorAll(".main-screen__item");
-    const middleImage = document.querySelectorAll(".main-screen__middle-image");
-
-    const bigImageTranslate = 170 / 50 * finalPosition;
-
-    bigImage.style.cssText = `
-      transform:  translateY(-${bigImageTranslate}%);
-    `;
-
-    const textsBlocksTranslate = 190 / 50 * finalPosition;
-
-    textsBlocks[0].style.cssText = `
-    transform: translate(-${textsBlocksTranslate}%, ${textsBlocksTranslate}%);
-    `;
-    textsBlocks[1].style.cssText = `
-    transform: translate(${textsBlocksTranslate}%, ${textsBlocksTranslate}%);
-    `;
-
-    const middleImageTranslate = 190 / 50 * finalPosition;
-    const middleImageScale = 1 + 2 / 50 * finalPosition
-    middleImage[0].style.cssText = `
-    transform: translate(-${middleImageTranslate}%, ${middleImageTranslate}%);
-    `;
-    middleImage[1].style.cssText = `
-    transform: translate(${middleImageTranslate}%, ${middleImageTranslate}%);
-    `;
-  }
-}
-
-document.addEventListener("mouseover", function (e) {
-  const elementTarget = e.target;
-  if (elementTarget.closest(".portfolio-swiper__button")) {
-    document.querySelectorAll(".portfolio-swiper__descr").forEach(function (s) {
-      s.classList.remove("portfolio-swiper-descr-active")
-    })
-    elementTarget.closest(".portfolio-swiper__descr").classList.add("portfolio-swiper-descr-active")
-  }
-  if (!elementTarget.closest(".portfolio-swiper-descr-active")) {
-    document.querySelectorAll(".portfolio-swiper__descr").forEach(function (s) {
-      s.classList.remove("portfolio-swiper-descr-active")
-    })
-  }
-})
-
-    function testWebP(callback) {
-  var webP = new Image();
-
-  webP.onload = webP.onerror = function () {
-    callback(webP.height == 2);
-  };
-
-  webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
-}
-
-testWebP(function (support) {
-  if (support == true) {
-    document.querySelector('body').classList.add('webp');
-  } else {
-    document.querySelector('body').classList.add('no-webp');
-  }
-});
-
-    const portfolioSwiperInitOne = document.querySelector(".portfolio-swiper-1");
-
-const portfolioSwiperInitTwo = document.querySelector(".portfolio-swiper-2");
-
-const portfolioSwiperInitThird = document.querySelector(".portfolio-swiper-3");
-
-if (portfolioSwiperInitOne) {
-  if (document.body.offsetWidth < 992) {
-    console.log("da")
-    const portfolioSwiperOne = new Swiper('.portfolio-swiper-1', {
-      observer: true,
-      observeParents: true,
-      watchOverflow: true,
-      direction: 'horizontal',
-      pagination: {
-        el: '.portfolio-swiper-pagination-1',
-        clickable: true,
-      },
-      breakpoints: {
-        320: {
-          slidesPerView: 1,
-          spaceBetween: 10
-        },
-        701: {
-          slidesPerView: 2,
-          spaceBetween: 10
-        }
-      },
-    });
-  }
-}
-
-if (portfolioSwiperInitTwo) {
-  if (document.body.offsetWidth < 992) {
-    console.log("da")
-    const portfolioSwiperTwo = new Swiper('.portfolio-swiper-2', {
-      observer: true,
-      observeParents: true,
-      watchOverflow: true,
-      direction: 'horizontal',
-      pagination: {
-        el: '.portfolio-swiper-pagination-2',
-        clickable: true,
-      },
-      breakpoints: {
-        320: {
-          slidesPerView: 1,
-          spaceBetween: 10
-        },
-        701: {
-          slidesPerView: 2,
-          spaceBetween: 10
-        }
-      },
-    });
-  }
-}
-
-if (portfolioSwiperInitThird) {
-  if (document.body.offsetWidth < 992) {
-    console.log("da")
-    const portfolioSwiperThird = new Swiper('.portfolio-swiper-3', {
-      observer: true,
-      observeParents: true,
-      watchOverflow: true,
-      direction: 'horizontal',
-      pagination: {
-        el: '.portfolio-swiper-pagination-3',
-        clickable: true,
-      },
-      breakpoints: {
-        320: {
-          slidesPerView: 1,
-          spaceBetween: 10
-        },
-        701: {
-          slidesPerView: 2,
-          spaceBetween: 10
-        }
-      },
-    });
-  }
-}
-
-    const aosInit = document.querySelector(".aos-init");
-if(aosInit) {
-  AOS.init({
-    duration: 700,
   });
-}
+
+  const portfolioSwiperInitOne = document.querySelector(".portfolio-swiper-1");
+
+  const portfolioSwiperInitTwo = document.querySelector(".portfolio-swiper-2");
+
+  if (portfolioSwiperInitOne) {
+    if (document.body.offsetWidth < 992) {
+      const portfolioSwiperOne = new Swiper('.portfolio-swiper-1', {
+        observer: true,
+        observeParents: true,
+        watchOverflow: true,
+        speed: 1000,
+        direction: 'horizontal',
+        pagination: {
+          el: '.portfolio-swiper-pagination-1',
+          clickable: true,
+        },
+        breakpoints: {
+          320: {
+            slidesPerView: 1,
+            spaceBetween: 10
+          },
+          701: {
+            slidesPerView: 2,
+            spaceBetween: 10
+          }
+        },
+      });
+    }
+  }
+
+  if (portfolioSwiperInitTwo) {
+    if (document.body.offsetWidth < 992) {
+      const portfolioSwiperTwo = new Swiper('.portfolio-swiper-2', {
+        observer: true,
+        observeParents: true,
+        watchOverflow: true,
+        speed: 1000,
+        direction: 'horizontal',
+        pagination: {
+          el: '.portfolio-swiper-pagination-2',
+          clickable: true,
+        },
+        breakpoints: {
+          320: {
+            slidesPerView: 1,
+            spaceBetween: 10
+          },
+          701: {
+            slidesPerView: 2,
+            spaceBetween: 10
+          }
+        },
+      });
+    }
+  }
+
 }
